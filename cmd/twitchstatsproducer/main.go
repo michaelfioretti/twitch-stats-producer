1c5522f1fdf1d5b853de083a172ea7ba77051dfe
Michael Fioretti <michaelfioretti1@gmail.com>
2024-05-06 16:52:39 -0700

    Docker: Add docker compose and pull kafka images

--- cmd/twitchstatsproducer/main.go
+++ cmd/twitchstatsproducer/main.go
@@ -1,15 +1,53 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"context"
+	"fmt"
+	"github.com/segmentio/kafka-go"
+	"time"
 )
 
+func produceMessages(brokerAddress string) {
+	topic := "test-topic"
+	partition := 0
+
+	conn, _ := kafka.DialLeader(context.Background(), "tcp", brokerAddress, topic, partition)
+
+	defer conn.Close()
+
+	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	conn.WriteMessages(
+		kafka.Message{Value: []byte("Hello World!")},
+	)
+
+	fmt.Println("Produced message")
+}
+
+func consumeMessages(brokerAddress string) {
+	topic := "test-topic"
+	partition := 0
+
+	conn, _ := kafka.DialLeader(context.Background(), "tcp", brokerAddress, topic, partition)
+
+	defer conn.Close()
+
+	conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+	batch := conn.ReadBatch(10e3, 1e6) // fetch 10KB min, 1MB max
+
+	b := make([]byte, 10e3) // 10KB max per message
+	for {
+		_, err := batch.Read(b)
+		if err != nil {
+			break
+		}
+		fmt.Println("Received message:", string(b))
+	}
+
+	batch.Close()
+}
+
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on
+	brokerAddress := "kafka:9092"
+	go produceMessages(brokerAddress)
+	consumeMessages(brokerAddress)
 }
